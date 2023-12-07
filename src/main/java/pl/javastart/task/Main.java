c7189db6a1c593879efb98884af0b357ec25aa97
AdamJaniczek <152891309+AdamJaniczek@users.noreply.github.com>
2023-12-07 19:24:00 +0100

    Część 2 zadania.

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,17 +3,8 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-        Phone iphone = new Phone();
-        iphone.model = "13mini";
-        iphone.manufacturer = "Apple";
-        iphone.colour = "red";
-        iphone.weight = 130;
-
-        Phone samsung = new Phone();
-        samsung.model = "A54";
-        samsung.manufacturer = "Samsung";
-        samsung.colour = "black";
-        samsung.weight = 190;
+        Phone iphone = new Phone("13mini", "Apple", "red", 120);
+        Phone samsung = new Phone("A54", "Samsung", "black", 190);
 
         System.out.printf("%s %s %s %d g.", iphone.model, iphone.manufacturer, iphone.colour, iphone.weight);
         System.out.println();
--- src/main/java/pl/javastart/task/Phone.java
+++ src/main/java/pl/javastart/task/Phone.java
@@ -1,6 +1,12 @@
 package pl.javastart.task;
 
 public class Phone {
+    Phone(String model, String manufacturer, String colour, int weight) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.colour = colour;
+        this.weight = weight;
+    }
     String model;
     String manufacturer;
     String colour;
